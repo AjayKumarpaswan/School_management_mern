cde271e8d765dde00fffd90a902a184fad83c336
Ajay Kumar <arvindkumar20102000@gmail.com>
2024-12-14 15:12:25 +0530

    Your commit message here

--- smsbackend/index.js
+++ smsbackend/index.js
@@ -1,31 +1,32 @@
-const express = require("express")
-const cors = require("cors")
-const mongoose = require("mongoose")
-const dotenv = require("dotenv")
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 // const bodyParser = require("body-parser")
-const app = express()
-const Routes = require("./routes/route.js")
+const app = express();
+const Routes = require("./routes/route.js");
 
-const PORT = process.env.PORT || 5000
+
 
 dotenv.config();
 
 // app.use(bodyParser.json({ limit: '10mb', extended: true }))
 // app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
 
-app.use(express.json({ limit: '10mb' }))
-app.use(cors())
-
+app.use(express.json({ limit: "10mb" }));
+app.use(cors());
+const PORT = process.env.PORT;
+//console.log("Momgo url",PORT);
 mongoose
-    .connect(process.env.MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(console.log("Connected to MongoDB"))
-    .catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log("NOT CONNECTED TO NETWORK", err));
 
-app.use('/', Routes);
+app.use("/", Routes);
 
 app.listen(PORT, () => {
-    console.log(`Server started at port no. ${PORT}`)
-})
+  console.log(`Server started at port no. ${PORT}`);
+});
