d6b62b34e9cd892cf4f187aaf0ba0ff31fb9ef12
Ajay Kumar <arvindkumar20102000@gmail.com>
2024-12-14 15:16:59 +0530

    Your commit message here

--- smsfrontend/src/pages/ChooseUser.js
+++ smsfrontend/src/pages/ChooseUser.js
@@ -17,7 +17,7 @@ import Popup from '../components/Popup';
 const ChooseUser = ({ visitor }) => {
   const dispatch = useDispatch()
   const navigate = useNavigate()
-  const password = "zxc"
+  const password = "12345"
 
   const { status, currentUser, currentRole } = useSelector(state => state.user);;
 
@@ -28,7 +28,7 @@ const ChooseUser = ({ visitor }) => {
   const navigateHandler = (user) => {
     if (user === "Admin") {
       if (visitor === "guest") {
-        const email = "yogendra@12"
+        const email = "ajay@ajay.com"
         const fields = { email, password }
         setLoader(true)
         dispatch(loginUser(fields, user))
@@ -41,7 +41,7 @@ const ChooseUser = ({ visitor }) => {
     else if (user === "Student") {
       if (visitor === "guest") {
         const rollNum = "1"
-        const studentName = "Dipesh Awasthi"
+        const studentName = "ashish"
         const fields = { rollNum, studentName, password }
         setLoader(true)
         dispatch(loginUser(fields, user))
@@ -53,7 +53,7 @@ const ChooseUser = ({ visitor }) => {
 
     else if (user === "Teacher") {
       if (visitor === "guest") {
-        const email = "tony@12"
+        const email = "anurag@anurag.com"
         const fields = { email, password }
         setLoader(true)
         dispatch(loginUser(fields, user))
